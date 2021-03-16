71cf7acaf2a00a51f80c0770e2936c9250d9f4e2
krisnaabdi <abdidevinjer@gmail.com>
2021-03-16 16:06:04 +0700

    taxi class

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_TAXI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai propertis
+            taxi.NamaPengemudi = "Udin";
+            taxi.SedangBertugas = true;
+            taxi.NomorPenumpang = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_TAXI
+{
+    class Taxi
+    {
+        //propertis
+        public string NamaPengemudi { get; set; }
+        public bool SedangBertugas { get; set; }
+        public int NomorPenumpang { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("NamaPengemudi : {0}", NamaPengemudi);
+            if (SedangBertugas == true)
+            {
+                Console.WriteLine("OnDuty : yes", SedangBertugas);
+            }
+            else
+            {
+                Console.WriteLine("OnDuty : no", SedangBertugas);
+            }
+            Console.WriteLine("NomorPenumPang : {0}", NomorPenumpang);
+        }
+           
+            
+        
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Udin sedang menjemput penumpang\n", NamaPengemudi);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Udin selesei mengantar penumpang\n", NamaPengemudi);
+        }
+    }
+}
